63aa0dbc1c512ad0deae828c066f000dce9de26b
shine1995 <shine.state7@gmail.com>
2017-02-01 13:30:30 -0500

    + dependencies. +  user entity

---
+++ src/main/java/com/shine/controllers/FirstWebsiteController.java
@@ -0,0 +1,11 @@
+package com.shine.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Blake on 2/1/17.
+ */
+
+@Controller
+public class FirstWebsiteController {
+}
---
+++ src/main/java/com/shine/entities/User.java
@@ -0,0 +1,13 @@
+package com.shine.entities;
+
+import javax.persistence.*;
+
+/**
+ * Created by Blake on 2/1/17.
+ */
+
+
+@Entity
+@Table(name = "users")
+public class User {
+}
