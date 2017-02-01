7c028b6b4ae89e24e8251cff21b5fb2cfb0a4cdc
shine1995 <shine.state7@gmail.com>
2017-02-01 10:41:03 -0500

    Initial Commit. Pushing from local repo.

---
+++ src/main/java/com/shine/FirstWebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.shine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstWebsiteApplication.class, args);
+	}
+}
